32962bbb9246c56b5a303b096e657d5f74677fec
camotio022 <123275976+camotio022@users.noreply.github.com>
2025-02-28 01:15:11 -0300

    TESTANDO LINKS

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -6,21 +6,39 @@ import { colors } from "../../colors/colors";
 export const Home = () => {
     return (
         <Tags.stack>
-            <Stack sx={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: '100%',
-                height: '60px',
-                backgroundColor: colors.background.overlay
-            }}>
-
-            </Stack>
-
+            <Tags.navbar>
+                <Stack sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'flex-start',
+                    ml: '20px',
+                    width: '30%',
+                    height: '90%',
+                    fontSize: { xs: "1.1rem", md: "1.5rem" },
+                    fontWeight: "900",
+                    fontFamily: "'Orbitron', sans-serif",
+                    textTransform: "uppercase",
+                    letterSpacing: "2px",
+                    lineHeight: '1.4'
+            
+                }}>
+                    {['T', 'I', 'M', 'O', 'F', 'I', 'C', 'I', 'A', 'L'].map((key, index) => (
+                        <Tags.strongtag sx={key === 'T'? {
+                            color: 'white',
+                            bgcolor: colors.text.secondary,
+                            height: '60%',
+                            padding: '4px',
+                            border: 'none'
+                        }:{}} key={index}>{key}</Tags.strongtag>
+                    ))}
+                </Stack>
+                <Stack>HOME</Stack>
+                <Stack>SOBRE MIM </Stack>
+                <Stack>SERVIÇOS</Stack>
+                <Stack>CONTATO </Stack>
+                <Stack>ASSINAR</Stack>
+            </Tags.navbar>
         </Tags.stack>
     )
 }
