7cde3a806b8d0fdb19d942d9f261563ee73b30c2
camotio022 <123275976+camotio022@users.noreply.github.com>
2025-02-28 01:14:58 -0300

    CRIANDO TAGS

--- src/pages/home/css.jsx
+++ src/pages/home/css.jsx
@@ -13,4 +13,24 @@ export const Tags = {
         overflowX: 'hidden',
         backgroundColor: colors.background.primary
     })),
+    navbar: styled(Stack)(({ }) => ({
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
+        height: '100px',
+        color: colors.text.title,
+    })),
+    strongtag: styled(Stack)(({ }) => ({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '1px',
+        lineHeight: '1.4',
+        borderBottom:`3px solid ${colors.text.secondary}` 
+    }))
 }
